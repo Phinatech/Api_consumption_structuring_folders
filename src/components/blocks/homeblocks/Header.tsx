1df36a8dcff7e19756baa12082747b0ac22038d2
Phinatech <ugbojudithchinemerem@gmail.com>
2023-04-03 16:04:50 +0100

    update

--- src/components/blocks/homeblocks/Header.tsx
+++ src/components/blocks/homeblocks/Header.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <h1>Header</h1>
+    </Container>
+  );
 };
 
 export default Header;
+const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: red;
+`;
